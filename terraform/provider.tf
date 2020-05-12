efa3c6741ac4cd3db9fbd6b6a8643a377ee723ee
Aydar Sharafiev <aydarsh@gmail.com>
2020-05-12 16:49:39 +0300

    configured google credentials

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = file("account.json")
+  credentials = base64decode(var.google_credentials)
   project     = "trip-analyzer-prj"
   region      = "us-central1"
   zone        = "us-central1-a"
