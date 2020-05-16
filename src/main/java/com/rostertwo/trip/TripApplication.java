71cd0deb1b8980c11add5c9726c2ece9c9d9fba6
Aidar Sharafiev <Aidar_Sharafiev@epam.com>
2020-05-16 19:40:03 +0300

    tested the complete CI/CD pipeline

--- src/main/java/com/rostertwo/trip/TripApplication.java
+++ src/main/java/com/rostertwo/trip/TripApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TripApplication {
 	@RequestMapping("/")
 	public String home() {
-		return "Hello World from Kubernetes";
+		return "Hello from Kubernetes";
 	}
 
 	public static void main(String[] args) {
