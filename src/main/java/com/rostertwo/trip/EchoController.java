32548f1816f5dc0d092717cff3a78ef65cb569a3
Aidar Sharafiev <Aidar_Sharafiev@epam.com>
2020-05-17 14:34:15 +0300

    defined basic auth example

--- src/main/java/com/rostertwo/trip/EchoController.java
+++ src/main/java/com/rostertwo/trip/EchoController.java
@@ -1,6 +1,7 @@
 package com.rostertwo.trip;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
