9901e6a28e9f57668f370170a335a1dcc9669a27
Aidar Sharafiev <Aidar_Sharafiev@epam.com>
2020-05-20 15:41:34 +0300

    added Basic Auth

--- src/main/java/com/rostertwo/trip/TripApiController.java
+++ src/main/java/com/rostertwo/trip/TripApiController.java
@@ -33,7 +33,7 @@ public class TripApiController {
     public static final long EPS = 1;
     private static final Logger log = LoggerFactory.getLogger(TripApiController.class);
     private final VehiclePushAnalysis output = new VehiclePushAnalysis();
-    @Value("${APIKEY:defaultKey}")
+    @Value("${APIKEY:defaultValue}")
     private String APIKEY;
     
 
